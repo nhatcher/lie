e6395c6164e628c25c66e73d2d76a9ca84ba7cab
Nicolás <nicoliere@gmail.com>
2012-02-07 20:54:50 +0100

    FIX: Do not know just now why it works

--- LiE/non-ANSI.c
+++ LiE/non-ANSI.c
@@ -2,7 +2,7 @@
 #include <unistd.h>  /* needed for |isatty| */
 #include <stdio.h> 
 
-int no_terminal(FILE* f) { return !isatty(fileno(f)); }
+int no_terminal(FILE* f) { return 0;/*!isatty(fileno(f));*/ }
 
 void sysinit(void)  {}
 
